2e204aa882e83bbed4e40a8dbecb5d219269341b
kayladong229 <105991864+kayladong229@users.noreply.github.com>
2022-07-17 20:31:09 -0700

    Edited classes in js and html files to be more consistent with css file

--- Develop/script.js
+++ Develop/script.js
@@ -38,18 +38,18 @@ var saveBtn = $('.saveBtn');
 //Create a function that saves time and event to local storage when save button is clicked
 saveBtn.on("click", function() {
     var time = $(this).parent().attr("id");
-    var event = $(this).siblings(".event").val();
+    var event = $(this).siblings(".description").val();
     localStorage.setItem(time, event);
 })
 
 // Retrieve any saved events upon refreshing
-$("#8 .event").val(localStorage.getItem("8"));
-$("#9 .event").val(localStorage.getItem("9"));
-$("#10 .event").val(localStorage.getItem("10"));
-$("#11 .event").val(localStorage.getItem("11"));
-$("#12 .event").val(localStorage.getItem("12"));
-$("#13 .event").val(localStorage.getItem("13"));
-$("#14 .event").val(localStorage.getItem("14"));
-$("#15 .event").val(localStorage.getItem("15"));
-$("#16 .event").val(localStorage.getItem("16"));
-$("#17 .event").val(localStorage.getItem("17"));
+$("#8 .description").val(localStorage.getItem("8"));
+$("#9 .description").val(localStorage.getItem("9"));
+$("#10 .description").val(localStorage.getItem("10"));
+$("#11 .description").val(localStorage.getItem("11"));
+$("#12 .description").val(localStorage.getItem("12"));
+$("#13 .description").val(localStorage.getItem("13"));
+$("#14 .description").val(localStorage.getItem("14"));
+$("#15 .description").val(localStorage.getItem("15"));
+$("#16 .description").val(localStorage.getItem("16"));
+$("#17 .description").val(localStorage.getItem("17"));
